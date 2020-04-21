6d186dd45427128205809b858721a8f6e9f8c2fa
Umar Haroon <umar.m.haroon@gmail.com>
2020-04-20 20:56:14 -0600

    adds a lot of UI changes and dynamic link loading

--- src/components/CreateHome/index.js
+++ src/components/CreateHome/index.js
@@ -1,3 +1,4 @@
+import qrcode from 'qrcode';
 import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import { compose } from 'recompose';
@@ -7,59 +8,103 @@ import { withFirebase } from '../Firebase';
 class CreateHomeBase extends Component {
     constructor(props) {
         super(props)
-        this.joinHomeButtonPressed = this.joinHomeButtonPressed.bind(this)
         this.joinHomeFormButtonPressed = this.joinHomeFormButtonPressed.bind(this)
+        this.createHomeButtonPressed = this.createHomeButtonPressed.bind(this)
+        this.createHomeFormButtonPressed = this.createHomeFormButtonPressed.bind(this)
     }
 
     updateInfo() {
         this.props.history()
     }
-
-    joinHomeButtonPressed() {
-        console.log("Button Test");
+    loadQR() {
+        if (this.props.firebase.auth.currentUser === null) { return }
+        var canvasElement = document.getElementById("qrcode")
+        var newURL = new URL("https://homelyweb.com/join/")
+        newURL.search = new URLSearchParams({ "user": this.props.firebase.auth.currentUser.uid })
+        var body = {
+            "dynamicLinkInfo": {
+                "domainUriPrefix": "https://homelyapp.page.link",
+                "link": newURL,
+                "iosInfo": {
+                    "iosBundleId": "com.Komodo.Homely",
+                },
+            },
+            "suffix": {
+                "option": "UNGUESSABLE"
+            }
+        }
+        var jsonBody = JSON.stringify(body)
+        fetch(`https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=${process.env.REACT_APP_API_KEY}`, {
+            method: 'POST',
+            body: jsonBody,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then((response) => {
+            response.json().then((result) => {
+                console.log(result)
+                qrcode.toCanvas(canvasElement, result.shortLink, { scale: 10 }, (error) => {
+                })
+            })
+        }).catch((error) => {
+            console.log("REJECTED")
+            console.log(error)
+        })
+    }
+    createHomeButtonPressed() {
         var container = document.getElementById("CreateHomeForm")
         container.hidden = !container.hidden
     }
     joinHomeFormButtonPressed(e) {
         console.log(e)
+        // var enteredID = document.getElementById("homeIDInput")
+        // this.props.firebase.joinHome()
         console.log("Form Button Test");
+
+        e.preventDefault()
+    }
+    createHomeFormButtonPressed(e) {
         e.preventDefault()
     }
 
     render() {
+
         return (
-           <center>
-               <div id="CreateHomeContainer"> <br></br>
-                <button type="button" onClick={() => { this.joinHomeButtonPressed() }} className="btn btn-link">
-                    Toggle Home
-                </button>
-                <br></br><br></br>
-                <form id="JoinHomeForm" hidden={false} onSubmit={this.joinHomeFormButtonPressed}>
-                    <div className="form-group">
-                        <label >
-                            Enter a Home ID to join a home
-                            <input type="text" className="form-control" id="homeIDInput" aria-describedby="homeID Help" placeholder="Home ID"></input>
-                        </label>
-                        <p><small><small>Put in the Home ID given to you by your roommate in order to connect to the Home.</small></small></p>
-                    </div>
-                    <input className="buttonStyle" type="submit" value="Join Home" />
-                </form > <br></br><br></br>
-                <button type="button" className="btn btn-link">
-                    Create a New Home
-                </button>
-                <br></br>
-                <form id="CreateHomeForm" hidden={false} onSubmit={this.joinHomeFormButtonPressed}>
-                    <div className="form-group">
-                        <label>
-                            Home Name (Optional)
+            <center>
+                <div id="CreateHomeContainer">
+                    <label>Have a roommate scan the QR Code to join a home</label>
+                    <br />
+
+                    <canvas id="qrcode" width="500px" height="500px"></canvas>
+                    {this.loadQR()}
+                    <br />
+                    <strong><p>OR</p></strong>
+                    <form id="JoinHomeForm" hidden={false} onSubmit={this.joinHomeFormButtonPressed}>
+                        <div className="form-group">
+                            <label >
+                                I have a link
+                            <small className="form-text text-muted">Enter the Home ID given to you by your roommate in order to connect to the Home.</small>
+                                <input type="text" className="form-control" id="homeIDInput" aria-describedby="homeID Help" placeholder="Home Link"></input>
+                            </label>
+                        </div>
+                        <button type="submit" className="btn btn-primary">Join Home</button>
+                    </form>
+                    <button type="button" className="btn btn-link" onClick={this.createHomeButtonPressed}>
+                        Create a New Home
+                    </button>
+                    <br></br>
+                    <form id="CreateHomeForm" hidden={true} onSubmit={this.joinHomeFormButtonPressed}>
+                        <div className="form-group">
+                            <label>
+                                Home Name (Optional)
                         <input type="email" className="form-control" id="HomeNameInput" aria-describedby="Home Name" placeholder="P. Sherman 42 Wallaby Way"></input>
-                        </label>
-                        <p><small><small>Input must be an email.</small></small></p>
-                    </div>
-                    <input className="buttonStyle" type="submit" value="Submit" />
-                </form >
-                <Link to={ROUTES.HOME}>Home</Link>
-            </div ></center>
+                            </label>
+                        </div>
+                        <input className="buttonStyle" type="submit" value="Submit" />
+                    </form >
+                    <Link to={ROUTES.HOME}>Home</Link>
+                </div>
+            </center >
         )
     }
 }
